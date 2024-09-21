a8b98d48340107fae91feb156c17703651ff3c49
yoyo1025 <hsjejxys@gmail.com>
2024-09-21 18:36:13 +0900

    プロンプトを変更

--- util/util.go
+++ util/util.go
@@ -10,7 +10,7 @@ import (
 
 func GenerateAIResponse(persona model.Persona, client *openai.Client) (string, error) {
 	prompt := fmt.Sprintf(
-		"次のペルソナの情報に基づいて、ペルソナが抱えている問題を解決するアドバイスを提供してください:\n"+
+		"次の情報は架空の困りごとをペルソナです。あなたはそのペルソナなり切ってください。ペルソナの設定はその都度付け加えても構いません。次の情報から軽く自己紹介をしてください。改行は要りません。:\n"+
 			"名前: %s\n性別: %s\n年齢: %d\n職業: %s\n問題: %s\n行動: %s\n",
 		persona.Name, persona.Sex, persona.Age, persona.Profession, persona.Problems, persona.Behavior,
 	)
