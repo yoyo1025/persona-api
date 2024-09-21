fb143c4abf84ba86380505cbc54be5869a8416f8
yoyo1025 <hsjejxys@gmail.com>
2024-09-21 12:18:59 +0900

    initDB関数用意

--- app/go.mod
+++ app/go.mod
@@ -3,5 +3,3 @@ module sample_app/app
 go 1.19
 
 require github.com/lib/pq v1.10.9
-
-require github.com/sashabaranov/go-openai v1.29.2 // indirect
--- app/main.go
+++ app/main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
+
+func initDB() {
+	var err error
+	connStr := fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_DATABASE"),
+	)
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+			log.Fatal("データベースへの接続に失敗しました：", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+			log.Fatal("データベースへの接続確認に失敗しました:", err)
+	}
+}
 
 func main() {
 	fmt.Println("now server started...")
+	initDB()
 
 	// シンプルなハンドラー
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
