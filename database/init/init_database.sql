38ec31bb143a227e038ce7d4dd5f59e52561de7b
yoyo1025 <hsjejxys@gmail.com>
2024-11-25 12:26:48 +0900

    DBの初期値設定

--- database/init/init_database.sql
+++ database/init/init_database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS sample_app;
+-- CREATE DATABASE IF NOT EXISTS persona_db;
 -- 1. usersテーブルの作成
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
@@ -6,11 +6,6 @@ CREATE TABLE IF NOT EXISTS users (
   email VARCHAR(100) UNIQUE NOT NULL,
   password VARCHAR(255)
 );
-
--- id=1のユーザーを挿入
-INSERT INTO users (id, name, email, password)
-VALUES (1, 'test1', 'test1@example.com', 'password')
-ON CONFLICT DO NOTHING;
 
 -- 2. personaテーブルの作成
 CREATE TABLE IF NOT EXISTS persona (
@@ -40,3 +35,44 @@ CREATE TABLE IF NOT EXISTS comment (
   is_user_comment BOOLEAN,
   good BOOLEAN DEFAULT FALSE
 );
+
+
+-- usersテーブルに5件のデータを挿入
+INSERT INTO users (id, name, email, password)
+VALUES 
+  (1, 'test1', 'test1@example.com', 'password1'),
+  (2, 'test2', 'test2@example.com', 'password2'),
+  (3, 'test3', 'test3@example.com', 'password3'),
+  (4, 'test4', 'test4@example.com', 'password4'),
+  (5, 'test5', 'test5@example.com', 'password5')
+ON CONFLICT DO NOTHING;
+
+-- personaテーブルに5件のデータを挿入
+INSERT INTO persona (id, name, user_id, sex, age, profession, problems, behavior)
+VALUES 
+  (1, 'Persona1', 1, 'male', 25, 'Engineer', 'Problem1', 'Behavior1'),
+  (2, 'Persona2', 2, 'female', 30, 'Designer', 'Problem2', 'Behavior2'),
+  (3, 'Persona3', 3, 'male', 22, 'Student', 'Problem3', 'Behavior3'),
+  (4, 'Persona4', 4, 'female', 27, 'Doctor', 'Problem4', 'Behavior4'),
+  (5, 'Persona5', 5, 'other', 35, 'Writer', 'Problem5', 'Behavior5')
+ON CONFLICT DO NOTHING;
+
+-- conversationテーブルに5件のデータを挿入
+INSERT INTO conversation (id, user_id, persona_id)
+VALUES 
+  (1, 1, 1),
+  (2, 2, 2),
+  (3, 3, 3),
+  (4, 4, 4),
+  (5, 5, 5)
+ON CONFLICT DO NOTHING;
+
+-- commentテーブルに5件のデータを挿入
+INSERT INTO comment (id, user_id, persona_id, comment, is_user_comment, good)
+VALUES 
+  (1, 1, 1, 'Comment1', TRUE, TRUE),
+  (2, 2, 2, 'Comment2', TRUE, FALSE),
+  (3, 3, 3, 'Comment3', FALSE, TRUE),
+  (4, 4, 4, 'Comment4', FALSE, FALSE),
+  (5, 5, 5, 'Comment5', TRUE, TRUE)
+ON CONFLICT DO NOTHING;
