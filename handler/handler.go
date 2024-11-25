620e0f99a1ef117b81421bb61130b5c409053a9c
yoyo1025 <hsjejxys@gmail.com>
2024-11-25 12:33:30 +0900

    handler.goをコメントアウト

--- handler/handler.go
+++ handler/handler.go
@@ -1,17 +1,17 @@
 package handler
 
-import (
-	"net/http"
+// import (
+// 	"net/http"
 
-	"github.com/yoyo1025/persona-api/database"
-)
+// 	"github.com/yoyo1025/persona-api/database"
+// )
 
-func ConversationHandler(w http.ResponseWriter, r *http.Request) {
-    if r.Method == http.MethodGet {
-        database.GetAllCommentsByID(w, r)
-    } else if r.Method == http.MethodPost {
-        database.PostComment(w, r)
-    } else {
-        http.Error(w, "許可されていないメソッドです", http.StatusMethodNotAllowed)
-    }
-}
+// func ConversationHandler(w http.ResponseWriter, r *http.Request) {
+//     if r.Method == http.MethodGet {
+//         database.GetAllCommentsByID(w, r)
+//     } else if r.Method == http.MethodPost {
+//         database.PostComment(w, r)
+//     } else {
+//         http.Error(w, "許可されていないメソッドです", http.StatusMethodNotAllowed)
+//     }
+// }
