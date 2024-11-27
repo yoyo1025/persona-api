06acbe5e34d55b96da501583cf0ef78fd7c57a68
yoyo1025 <hsjejxys@gmail.com>
2024-11-27 09:38:13 +0900

    初期データ挿入時にidを削除

--- database/init/init_database.sql
+++ database/init/init_database.sql
@@ -38,41 +38,41 @@ CREATE TABLE IF NOT EXISTS comment (
 
 
 -- usersテーブルに5件のデータを挿入
-INSERT INTO users (id, name, email, password)
+INSERT INTO users (name, email, password)
 VALUES 
-  (1, 'test1', 'test1@example.com', 'password1'),
-  (2, 'test2', 'test2@example.com', 'password2'),
-  (3, 'test3', 'test3@example.com', 'password3'),
-  (4, 'test4', 'test4@example.com', 'password4'),
-  (5, 'test5', 'test5@example.com', 'password5')
+  ('test1', 'test1@example.com', 'password1'),
+  ('test2', 'test2@example.com', 'password2'),
+  ('test3', 'test3@example.com', 'password3'),
+  ('test4', 'test4@example.com', 'password4'),
+  ('test5', 'test5@example.com', 'password5')
 ON CONFLICT DO NOTHING;
 
 -- personaテーブルに5件のデータを挿入
-INSERT INTO persona (id, name, user_id, sex, age, profession, problems, behavior)
+INSERT INTO persona (name, user_id, sex, age, profession, problems, behavior)
 VALUES 
-  (1, 'Persona1', 1, 'male', 25, 'Engineer', 'Problem1', 'Behavior1'),
-  (2, 'Persona2', 2, 'female', 30, 'Designer', 'Problem2', 'Behavior2'),
-  (3, 'Persona3', 3, 'male', 22, 'Student', 'Problem3', 'Behavior3'),
-  (4, 'Persona4', 4, 'female', 27, 'Doctor', 'Problem4', 'Behavior4'),
-  (5, 'Persona5', 5, 'other', 35, 'Writer', 'Problem5', 'Behavior5')
+  ('Persona1', 1, 'male', 25, 'Engineer', 'Problem1', 'Behavior1'),
+  ('Persona2', 2, 'female', 30, 'Designer', 'Problem2', 'Behavior2'),
+  ('Persona3', 3, 'male', 22, 'Student', 'Problem3', 'Behavior3'),
+  ('Persona4', 4, 'female', 27, 'Doctor', 'Problem4', 'Behavior4'),
+  ('Persona5', 5, 'other', 35, 'Writer', 'Problem5', 'Behavior5')
 ON CONFLICT DO NOTHING;
 
 -- conversationテーブルに5件のデータを挿入
-INSERT INTO conversation (id, user_id, persona_id)
+INSERT INTO conversation (user_id, persona_id)
 VALUES 
-  (1, 1, 1),
-  (2, 2, 2),
-  (3, 3, 3),
-  (4, 4, 4),
-  (5, 5, 5)
+  (1, 1),
+  (2, 2),
+  (3, 3),
+  (4, 4),
+  (5, 5)
 ON CONFLICT DO NOTHING;
 
 -- commentテーブルに5件のデータを挿入
-INSERT INTO comment (id, user_id, persona_id, comment, is_user_comment, good)
+INSERT INTO comment (user_id, persona_id, comment, is_user_comment, good)
 VALUES 
-  (1, 1, 1, 'Comment1', TRUE, TRUE),
-  (2, 2, 2, 'Comment2', TRUE, FALSE),
-  (3, 3, 3, 'Comment3', FALSE, TRUE),
-  (4, 4, 4, 'Comment4', FALSE, FALSE),
-  (5, 5, 5, 'Comment5', TRUE, TRUE)
+  (1, 1, 'Comment1', TRUE, TRUE),
+  (2, 2, 'Comment2', TRUE, FALSE),
+  (3, 3, 'Comment3', FALSE, TRUE),
+  (4, 4, 'Comment4', FALSE, FALSE),
+  (5, 5, 'Comment5', TRUE, TRUE)
 ON CONFLICT DO NOTHING;
